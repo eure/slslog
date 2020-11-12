6c85bce1113d8a06baf0acfaa4d283bbaa45c2ec
emahiro <ema.hiro@gmail.com>
2020-11-12 12:25:52 +0900

    describe godoc in span

--- span.go
+++ span.go
@@ -6,11 +6,18 @@ import (
 	"go.opencensus.io/trace"
 )
 
+// slslog.Span is wrapper of go.opencensus.io/trace.Span.
 type Span struct {
 	ctx  context.Context
 	span *trace.Span
 }
 
+// StartSpan starts a new span of the current span in the context.
+//
+// This returns the new span which has a new child span and the context.
+// which contains the newly created span in the span, and you are able
+// to use this context to propagate the returned span in process per lambda
+// call.
 func StartSpan(ctx context.Context, label string) *Span {
 	ctx, span := trace.StartSpan(ctx, label)
 	return &Span{
@@ -19,10 +26,12 @@ func StartSpan(ctx context.Context, label string) *Span {
 	}
 }
 
+// Context returns the span context with the given Span attached.
 func (s *Span) Context() context.Context {
 	return s.ctx
 }
 
+// End ends the span.
 func (s *Span) End() {
 	s.span.End()
 }
